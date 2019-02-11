5959c9d46f768896a409b5357a40894a688803ed
LEE010 <35730837+LEE010@users.noreply.github.com>
2019-02-12 00:29:52 +0900

    model

---
+++ second_project/blog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    pub_data = models.DateTimeField("data published")
+    body = models.TextField()
