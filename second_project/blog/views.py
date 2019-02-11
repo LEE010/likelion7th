55262f61c9cee4e51c51bf4db7403e22bb4bd76e
LEE_DONG_HYUN <dhlee318@me.com>
2019-02-12 02:18:09 +0900

    queryset & method

--- second_project/blog/views.py
+++ second_project/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Blog
 
-# Create your views here.
+def home(request):
+    blogs_all = Blog.objects.all() # query set!
+    blogs = Blog.objects
+    return render(request, 'home.html',{'blogs':blogs,'blogs_all':blogs_all})
